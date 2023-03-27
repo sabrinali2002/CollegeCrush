e5a35ef3323eb2f07515ee8ed71e1ea4cbaa043d
Sabrina Li <sabrinatli2002@gmail.com>
2023-03-27 19:33:12 -0400

    a

--- backend/app.py
+++ backend/app.py
@@ -40,8 +40,8 @@ def home():
 
 @app.route("/colleges")
 def episodes_search():
-	f = open('\colleges.json')
-	# file = json.load(f)
+	f = open('colleges.json')
+	file = json.load(f)
     text = request.args.get("title")
     return sql_search(text)
 
