59a15b292aa8adf3005d4ab29042a01ed769939d
jayfeng20 <jonathanfeng2001@gmail.com>
2023-03-27 17:12:12 -0400

    changed app.py

--- backend/app.py
+++ backend/app.py
@@ -14,7 +14,7 @@ os.environ["ROOT_PATH"] = os.path.abspath(os.path.join("..", os.curdir))
 MYSQL_USER = "root"
 MYSQL_USER_PASSWORD = "Youyou0305!"
 MYSQL_PORT = 3306
-MYSQL_DATABASE = "kardashiandb"
+MYSQL_DATABASE = "clush"
 
 mysql_engine = MySQLDatabaseHandler(
     MYSQL_USER, MYSQL_USER_PASSWORD, MYSQL_PORT, MYSQL_DATABASE
