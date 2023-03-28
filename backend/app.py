1df1725cfa26d3b611d87e3d33c738be939cfeba
Sabrina Li <sabrinatli2002@gmail.com>
2023-03-28 14:52:20 -0400

    blah

--- backend/app.py
+++ backend/app.py
@@ -52,7 +52,7 @@ def search_similarity(data, queries):
     for colleges in data:
         if 'state' in colleges and colleges['state'] == dic['state'] and int(colleges['tot_enroll'])>1000:
             arr.append(({'title': colleges['name'], 'website': colleges['website'],'enrolled': colleges['tot_enroll']}))
-        elif 'city' in colleges and (colleges['city'].lower()) == dic['city'].lower():
+        elif 'city' in colleges and'city' in dic and colleges['city'].lower() == dic['city'].lower():
             arr.append(({'title': colleges['name'], 'website': colleges['website'],'enrolled': colleges['tot_enroll']}))
     newlist = sorted(arr, key=lambda d: d['title']) 
     return newlist
