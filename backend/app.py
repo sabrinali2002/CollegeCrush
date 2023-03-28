b42383299ca718602b788aac4856a61fa09c3ac0
Sabrina Li <sabrinatli2002@gmail.com>
2023-03-28 15:11:12 -0400

    blah

--- backend/app.py
+++ backend/app.py
@@ -50,11 +50,10 @@ def search_similarity(data, queries):
         else:
             dic['city'] = i
     for colleges in data:
-        if 'city' in colleges and colleges['city'].lower() == queries.lower() and int(colleges['tot_enroll'])>1000:
+        if 'city' in colleges and'city' in dic and colleges['city'].lower() == queries.lower() and int(colleges['tot_enroll'])>1000:
             arr.append(({'title': colleges['name'], 'website': colleges['website'],'enrolled': colleges['tot_enroll']}))
-        elif 'state' in colleges and colleges['state'] == dic['state'] and int(colleges['tot_enroll'])>1000:
-            arr.append(({'title': colleges['name'], 'website': colleges['website'],'enrolled': colleges['tot_enroll']}))
-    newlist = sorted(arr, key=lambda d: d['title'])
+       
+    newlist = sorted(arr, key=lambda d: d['title']) 
     return newlist
 @app.route("/")
 def home():
