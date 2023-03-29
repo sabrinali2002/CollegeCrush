193c901150f09c4b85d93059baee2727a70c04d0
Sabrina Li <sabrinatli2002@gmail.com>
2023-03-29 01:00:59 -0400

    blah

--- backend/app.py
+++ backend/app.py
@@ -52,10 +52,11 @@ def search_similarity(data, queries, size):
         else:
             dic['city'] = i
     for colleges in data:
-        if 'city' in dic and colleges['city'].lower() == dic['city'].lower() and int(colleges['tot_enroll'])>1000:
-            arr.append(({'title': colleges['name'], 'location': colleges['city']+", "+colleges['state'],'enrolled':size,'website': colleges['website']}))
-        if queries.upper() == colleges['state']:
-            arr.append(({'title': colleges['name'], 'location': colleges['city']+", "+colleges['state'],'enrolled':colleges['tot_enroll'],'website': colleges['website']}))
+        if (size == "small" and colleges['tot_enroll'] <= 5000) or (size == "medium" and colleges['tot_enroll'] <= 15000) or (size == "large" and colleges['tot_enroll'] > 15000):
+            if 'city' in dic and colleges['city'].lower() == dic['city'].lower() and int(colleges['tot_enroll'])>1000:
+                arr.append(({'title': colleges['name'], 'location': colleges['city']+", "+colleges['state'],'enrolled':colleges['tot_enroll'],'website': colleges['website']}))
+            if queries.upper() == colleges['state']:
+                arr.append(({'title': colleges['name'], 'location': colleges['city']+", "+colleges['state'],'enrolled':colleges['tot_enroll'],'website': colleges['website']}))
     newlist = sorted(arr, key=lambda d: d['title']) 
     return newlist
 @app.route("/")
