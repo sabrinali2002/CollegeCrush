52cd9725825807a258b7302a6e12c5dd8e1a3a3a
Vy <vn46@cornell.edu>
2023-04-18 17:22:43 -0400

    db cred

--- backend/app.py
+++ backend/app.py
@@ -19,9 +19,10 @@ os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..", os.curdir))
 # MYSQL_DATABASE = "colleges"
 
 MYSQL_USER = "root"
-MYSQL_USER_PASSWORD = "MayankRao16Cornell.edu"
+# MYSQL_USER_PASSWORD = "MayankRao16Cornell.edu"
+MYSQL_USER_PASSWORD = ""
 MYSQL_PORT = 3306
-MYSQL_DATABASE = "colleges"
+MYSQL_DATABASE = "kardashiandb"
 mysql_engine = MySQLDatabaseHandler(
     MYSQL_USER, MYSQL_USER_PASSWORD, MYSQL_PORT, MYSQL_DATABASE)
 
